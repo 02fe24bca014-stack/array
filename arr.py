5599b4ee6c3db38f0c5187713c722ef59deb1720
02fe24bca014-stack <02fe24bca014@kletech.ac.in>
2026-01-03 09:45:12 +0530

    Create arr.py

---
+++ arr.py
@@ -0,0 +1,8 @@
+def main():
+  scores = [76, 82, 72, 92]
+  total = sum(scores)
+  average = total / len(scores)
+  print("=== main/master branch output ===")
+  print(f"scores: {scores}")
+  print(f"sum: {total}")
+  print(f"Average: {average}")
